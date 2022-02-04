8de478ed66b82e9c82e9caa7bee5e1bb8dfa71b5
Ralf Schneider <ralf.schneider@hlrs.de>
2022-02-04 12:05:01 +0100

    Added default parameter space.

---
+++ R/param.space.R
@@ -0,0 +1,15 @@
+#' Parameter space
+#'
+#' Data structure to store model parameters which are protentialy subject to
+#' variations.
+#'
+#' @docType data
+#'
+#' @usage data(param.space)
+#'
+#' @format An data structure whose components can be initialized by calls to
+#' \code{set.pspace}.
+#'
+#' @keywords datasets
+#'
+"param.space"
